36a40c319079a26e05de9359985edadfada15b5d
Tarcisio Coutinho <tcs5cin@gmail.com>
2024-12-10 21:06:41 -0300

    initial commit

---
+++ src/main/java/io/tacsio/spring/kubernetes/probes/ProbesApplication.java
@@ -0,0 +1,13 @@
+package io.tacsio.spring.kubernetes.probes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProbesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProbesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/tacsio/spring/kubernetes/probes/ProbesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.tacsio.spring.kubernetes.probes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProbesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
